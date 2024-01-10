9319a5b1d94353b0e87df78cfe4e806f10a529e9
squadri-qt <info@quadtechnology.co.uk>
2024-01-10 10:37:39 +0000

    commit blog template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://astro-blog-teal-five.vercel.app',
+  integrations: [preact()]
+});
