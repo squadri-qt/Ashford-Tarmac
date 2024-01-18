9c1007dfddc5d43965af1504e7d019ce8ac8a697
squadri-qt <info@quadtechnology.co.uk>
2024-01-18 18:32:30 +0000

    homepage complete

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,31 @@
 export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
-      extend: {},
+      borderWidth: {
+        DEFAULT: '1px',
+      },
+      extend: {
+        fontFamily: {
+          'sans': ['Roboto', 'ui-sans-serif', 'system-ui'],
+          'serif': ['Roboto Slab', 'ui-serif'],
+          'mono': ['Roboto Mono', 'ui-monospace'],
+        },
+        colors: {
+          'gray-dark': '#2B2B2B',
+          'gray-border': '#3A3A3A',
+          'gray': '#8E8E8E',
+          'gray-light': '#F2F2F2',
+          'red': '#ED1C24',
+          'red-dark': '#D2131B',
+        },
+        width: {
+          '50': '58px',
+        },
+        boxShadow:{
+          '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+          '4xl': '0 35px 60px -15px rgba(0, 0, 0, 0.5)',
+        },      
+      },
     },
     plugins: [],
   };
