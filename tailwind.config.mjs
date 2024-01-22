d19896b77e064683cb72e96baae57cd88723a535
squadri-qt <info@quadtechnology.co.uk>
2024-01-22 19:21:15 +0000

    redone all stles ready for responsvive settings

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,73 @@
 export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
+      fontSize: {
+        xs: ['0.6rem', {
+          lineHeight: '1.2rem',
+          letterSpacing: '0.05rem',
+          fontWeight: '500',
+        }],
+        sm: ['0.8rem', {
+          lineHeight: '1.2rem',
+          letterSpacing: '0.05rem',
+          fontWeight: '500',
+        }],
+        base: ['1rem', {
+          lineHeight: '1.4rem',
+          letterSpacing: '0.05rem',
+          fontWeight: '400',
+        }],
+        lg: ['1rem', {
+          lineHeight: '1.5rem',
+          letterSpacing: '0.05rem',
+          fontWeight: '600',
+        }],
+        xl: ['1.25rem', {
+          lineHeight: '1.5rem',
+          letterSpacing: '0.05rem',
+          fontWeight: '500',
+        }],
+        '2xl': ['1.5rem', {
+          lineHeight: '2rem',
+          letterSpacing: '0.05rem',
+          fontWeight: '500',
+        }],
+        '3xl': ['1.953rem', {
+          lineHeight: '2.5rem',
+          letterSpacing: '0.02rem',
+          fontWeight: '700',
+        }],
+        '3x2': ['1.8rem', {
+          lineHeight: '2rem',
+          letterSpacing: '0.06rem',
+          fontWeight: '900',
+        }],
+        '4xl': ['2.441rem', {
+          lineHeight: '1.5rem',
+          letterSpacing: '0.05rem',
+          fontWeight: '700',
+        }],
+        '5xl': ['3.052rem', {
+          lineHeight: '1.5rem',
+          letterSpacing: '0.05rem',
+          fontWeight: '700',
+        }],
+        '6xl': ['4.2rem', {
+          lineHeight: '4rem',
+          letterSpacing: '-0.04rem',
+          fontWeight: '300',
+        }],
+        '7xl': ['7rem', {
+          lineHeight: '7rem',
+          letterSpacing: '-0.04rem',
+          fontWeight: '900',
+        }],
+        'thint': ['2rem', {
+          lineHeight: '2rem',
+          letterSpacing: '0.01rem',
+          fontWeight: '100',
+        }],
+      },
       borderWidth: {
         DEFAULT: '1px',
       },
@@ -25,8 +92,11 @@ export default {
         boxShadow:{
           '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
           '4xl': '0 35px 60px -15px rgba(0, 0, 0, 0.5)',
-        },      
+        },
       },
     },
-    plugins: [],
+    plugins: [
+      "prettier-plugin-astro",
+      "prettier-plugin-tailwindcss"
+    ],
   };
