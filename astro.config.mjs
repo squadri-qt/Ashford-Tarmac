489004280e5a5042696d8121df0467ec10e3ab0b
quad-cw <quad-cw@ct1.xyz>
2024-03-17 17:53:40 +0000

    Tailwind duplicate styles fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://at-gamma.vercel.app/',
-  integrations: [preact(), tailwind(),],
+  integrations: [preact(),],
   prefetch: {prefetchAll: true},
 });
