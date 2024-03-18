c7e1d9fe2eb75e1c653e7e5625b67b4cdde0934f
quad-cw <quad-cw@ct1.xyz>
2024-03-18 16:55:07 +0000

    CSP stylesheet fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    inlineStylesheets: 'never'
+  },
   site: 'https://at-gamma.vercel.app/',
   integrations: [preact(),],
   prefetch: {prefetchAll: true},
